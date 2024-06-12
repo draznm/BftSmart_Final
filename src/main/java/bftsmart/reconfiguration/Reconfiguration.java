9e9a09a86626793dc3aab0680040cbc2a8c28541
tejas <tmane002@ucr.edu>
2024-06-12 03:15:00 -0700

    checking state construct shortcut

--- src/main/java/bftsmart/reconfiguration/Reconfiguration.java
+++ src/main/java/bftsmart/reconfiguration/Reconfiguration.java
@@ -79,7 +79,7 @@ public class Reconfiguration {
 //        byte[] reply = proxy.invoke(TOMUtil.getBytes(request), TOMMessageType.RECONFIG);
 
         System.out.println("sending RECONFIG message");
-        byte[] reply = proxy.invokeWithTimeout(TOMUtil.getBytes(request), TOMMessageType.RECONFIG, 20);
+        byte[] reply = proxy.invokeWithTimeout(TOMUtil.getBytes(request), TOMMessageType.RECONFIG, 6);
 
         request = null;
         

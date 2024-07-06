0c764945b7f11610fb3b340d298ce5fe4ae3dd68
tejas <tmane002@ucr.edu>
2024-07-06 14:50:21 -0700

    testing reconfig perf

--- src/main/java/bftsmart/demo/ycsb/YCSBClient.java
+++ src/main/java/bftsmart/demo/ycsb/YCSBClient.java
@@ -226,7 +226,7 @@ public class YCSBClient extends DB {
 
         boolean successfull_reconfig = true;
 
-        int upperLimit = 81;
+        int upperLimit = 82;
         
         if ((TxnCounter> upperLimit)&& (ClientID==4))
         {
